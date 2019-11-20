bb9115607e108f8f71627bf4338d009512811c1b
OnikenX <superjmm123@gmail.com>
2019-11-20 05:15:01 +0000

    corrigido segment fault

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ bool cmpcmd(char * longcmd, char * shortcmd, char * compare){
 
 int cmdlineprinc()
 {
-  char * raw = NULL;
+  char  raw[200];
   char * argv[10];
   int argc = 0 ;
   printf ( "Bem vindo a linha de comandos do gestor do msgdist,\n insira help ou h para pedir ajuda e dicas.\n" );
@@ -38,7 +38,7 @@ int cmdlineprinc()
   while ( std::fgets ( raw ,200, stdin ) )
     {
       if (raw[0]!='\n' )
-        {
+        { 
           argc=0;
           raw[std::strlen(raw) - 1] = '\0'; //trocar o \n por \0 para dizer o limite da cmd
           argv[argc] = std::strtok( raw, " " );
